a0ae654b1d094ea1e628d8b78f297c333dca628a
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-09-24 12:22:01 +0000

    First commit

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,6 @@
 //To launch the jquery once the page is loaded (to avoid issues)
-$(document).ready(function () {
 
+$(document).ready(function () {
+    $('h2').addClass("underline");
+    $('ul').removeClass("border");
 });
